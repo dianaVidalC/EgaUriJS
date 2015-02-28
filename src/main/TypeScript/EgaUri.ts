bba0dbccbd40f373f976821c432d6f154ec55005
EgaTuts <egatuts@gmail.com>
2015-02-28 18:33:07 +0100

    Added TypeScript Gulp Compiler and TypeScript files.

---
+++ src/main/TypeScript/EgaUri.ts
@@ -0,0 +1,29 @@
+/*
+ *  Module imports.
+ */
+import Scheme    = require("./modules/Scheme");
+import Subdomain = require("./modules/Subdomain");
+import Domain    = require("./modules/Domain");
+import Username  = require("./modules/Username");
+import Password  = require("./modules/Password");
+import Port      = require("./modules/Port");
+import Path      = require("./modules/Path");
+import Query     = require("./modules/Query");
+import Fragment  = require("./modules/Fragment");
+
+export class EgaUri {
+
+  /*
+   *  Submodules importing.
+   */
+  public static Scheme    = Scheme;
+  public static Subdomain = Subdomain;
+  public static Domain    = Domain;
+  public static Username  = Username;
+  public static Password  = Password;
+  public static Port      = Port;
+  public static Path      = Path;
+  public static Query     = Query;
+  public static Fragment  = Fragment;
+
+}
